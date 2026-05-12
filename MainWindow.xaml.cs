6a7ba124c0d7588bde708dbcc181af2f58a8c55d
1202825 <1202825@noreply.localhost>
2026-05-12 15:53:00 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,10 +15,62 @@ namespace WpfApp3
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    public class Song
+    {
+        public string cover;
+        public string title;
+        public string artist;
+        public string album;
+        public string genre;
+        public string duration;
+        public Song(string _cover,string _title, string _artist, string _album, string _genre, string _duration) {
+            cover= _cover;
+            title= _title;
+            artist= _artist;
+                
+            album= _album;
+            genre= _genre;
+            duration= _duration;
+                
+
+        }
+    }
+    public class Playlist
+    {
+        public string cover;
+        public string name;
+        public Playlist(string _cover, string _name) { 
+            cover
+                = _cover;
+            name = _name;
+        }
+    }
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Song> Songs { get; set; }
+        public ObservableCollection<Playlist> Playlists { get; set; }
         public MainWindow()
         {
+            Songs = new ObservableCollection<Song>
+            {
+                new Song(
+                    "_cover",
+                    "_title",
+                     "_artist",
+                    "_album",
+                    "_genre",
+                    "_duration"
+                ),
+                new Song(
+                    "_cover",
+                    "_title",
+                     "_artist",
+                    "_album",
+                    "_genre",
+                    "_duration"
+                )
+            };
             InitializeComponent();
         }
     }
